035c5e8a8a8e09138ce11667aee961529c5f4f8e
Kuanysh Aptaizhanov <89320116+kuanysh-1998@users.noreply.github.com>
2023-07-10 00:55:51 +0600

    initial

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  foods: Food[] = [];
+  constructor(
+    private foodService: FoodService,
+    private activatedRoot: ActivatedRoute
+  ) {
+    this.activatedRoot.params.subscribe((params) => {
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySearchTerm(
+          params.searchTerm
+        );
+      else this.foods = this.foodService.getAll();
+    });
+    this.foods = foodService.getAll();
+  }
+
+  ngOnInit(): void {}
+}
