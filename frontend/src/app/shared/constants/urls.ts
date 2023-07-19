9a75dd4ef666bd435779b9224c592b1eb1c603a5
kuanysh-1998 <kuanysh.aptayzhanov@mail.ru>
2023-07-19 10:29:26 +0600

    changed cors url

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://foodstore-wd4c.onrender.com';
 
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
